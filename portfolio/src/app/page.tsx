9b4f43b6d80900c36885b520a7e508e18cca52d4
Dx-B <billyzhangdx@gmail.com>
2026-03-12 20:30:23 -0400

    initial commit

---
+++ portfolio/src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <header>
+        <h1>Welcome!</h1>
+      </header>
+      <body>
+        <p>
+          Hello World!
+        </p>
+      </body>
+    </div>
+  );
+}
