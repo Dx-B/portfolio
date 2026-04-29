4e52581addc98265cdf4181bb643a46484f9f62d
Dx-B <billyzhangdx@gmail.com>
2026-04-29 00:19:07 -0400

    new

--- mainpage/src/app/temp/page.tsx
+++ mainpage/src/app/temp/page.tsx
@@ -67,7 +67,7 @@ export default function Home() {
                       bg-white/60
                     "
                   >
-                    Get Started
+                    Get Started Um
                   </button>
                 </Link>
                 
